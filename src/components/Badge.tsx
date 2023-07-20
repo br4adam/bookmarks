9891e472b9fb375c02b2d648c799f2c59ff787c7
br4adam <br4adam@gmail.com>
2023-07-20 11:47:53 +0200

    add a badge to notify users about the new domain

---
+++ src/components/Badge.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import { Cancel } from "iconoir-react"
+
+const Badge = () => {
+  const [ isOpen, setIsOpen ] = useState<boolean>(true)
+
+  if (window.location.hostname === "kmarks.boo" || !isOpen) return null
+
+  return (
+    <span className="relative inline-block w-fit mx-auto overflow-hidden rounded-full p-[1px]">
+      <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#71717a_0%,#27272a_50%,#71717a_100%)]" />
+      <div className="flex gap-1 h-full w-full items-center justify-center rounded-full bg-zinc-950 px-3 py-1 text-sm text-zinc-200 backdrop-blur-3xl">
+        <p>From now on, please use <a href="https://kmarks.boo" className="font-bold underline">kmarks.boo</a>! &#127881;</p>
+        <Cancel className="cursor-pointer" width={16} onClick={() => setIsOpen(false)}/>
+      </div>
+    </span>
+  )
+}
+
+export default Badge
--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,11 +1,13 @@
 import Balancer from "react-wrap-balancer"
+import Badge from "./Badge"
 
 const Headline = () => {
   return (
     <div className="flex flex-col gap-6 mt-20 mb-8 text-center text-transparent from-zinc-400 via-zinc-200 to-zinc-400 bg-gradient-to-r bg-clip-text animate-fade-down animate-duration-500">
-      <Balancer>
-        <h1 className="sm:text-6xl text-5xl font-bold md:text-7xl max-w-5xl">Say goodbye to messy bookmarks</h1>
-      </Balancer>
+      <Badge />
+      <h1 className="sm:text-6xl text-5xl font-bold md:text-7xl max-w-5xl">
+        <Balancer>Say goodbye to messy bookmarks</Balancer>
+      </h1>
       <p className="text-lg break-words">Organize your online life effortlessly!</p>
     </div>
   )
