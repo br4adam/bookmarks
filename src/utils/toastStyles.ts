b5319d5b9ed8f364d1f967d98657548c6f24f34f
br4adam <br4adam@gmail.com>
2024-09-01 14:23:19 +0200

    fix toast style colors

--- src/utils/toastStyles.ts
+++ src/utils/toastStyles.ts
@@ -6,5 +6,5 @@ const createToastStyle = (bgColor: string, textColor: string, borderColor: strin
 })
 
 export const defaultToastStyle = createToastStyle("zinc-900", "zinc-200", "zinc-700")
-export const successToastStyle = createToastStyle("#001f0f", "#59f3a6", "#003d1c")
-export const errorToastStyle = createToastStyle("#2d0607", "#ff9ea1", "#4d0408")
+export const successToastStyle = createToastStyle("[#001f0f]", "[#59f3a6]", "[#003d1c]")
+export const errorToastStyle = createToastStyle("[#2d0607]", "[#ff9ea1]", "[#4d0408]")
