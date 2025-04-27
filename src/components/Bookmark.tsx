ea51445137c7984bb626236dfef6896dc0783259
br4adam <br4adam@gmail.com>
2025-04-27 20:34:45 +0200

    fix issue with bookmark image not loading properly

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,11 +1,11 @@
+import { SyntheticEvent } from "react"
 import { useBookmarkStore } from "../stores/BookmarkStore"
 import { Pin } from "iconoir-react"
-import fallbackImage from "../assets/fallback.png"
 import BookmarkTags from "./BookmarkTags"
 import BookmarkOptions from "./BookmarkOptions"
 import Skeleton from "./Skeleton"
 import CardSpotlight from "./CardSpotlight"
-import useProgressiveImage from "../hooks/useProgressiveImage"
+import useIntersectionObserver from "../hooks/useIntersectionObserver"
 
 type Props = {
   bookmark: Bookmark
@@ -13,25 +13,25 @@ type Props = {
 
 const Bookmark = ({ bookmark }: Props) => {
   const loading = useBookmarkStore(state => state.loading)
+  const { ref, isVisible } = useIntersectionObserver()
 
-  const { imageSrc, isLoading: bookmarkImageIsLoading, imageRef } = useProgressiveImage({ src: bookmark.image, fallbackSrc: fallbackImage })
-  const { imageSrc: faviconSrc, isLoading: bookmarkFaviconIsLoading, imageRef: faviconRef } = useProgressiveImage({ src: `https://icon.horse/icon/${bookmark.domain}`, fallbackSrc: fallbackImage })
+  const addImageFallback = (event: SyntheticEvent<HTMLImageElement, Event>) => {
+    event.currentTarget.src = "./fallback.png"
+  }
 
   if (loading) return <Skeleton />
 
   return (
     <CardSpotlight className="p-0">
-      <div ref={imageRef as React.RefObject<HTMLDivElement>} className="z-10 relative aspect-[1.91/1] bg-zinc-800 rounded-t-[11px] overflow-hidden">
-        <img className={`object-cover size-full m-auto ease-out duration-300 ${bookmarkImageIsLoading ? 'opacity-0' : 'opacity-100'}`} src={imageSrc} alt={bookmark.title} />
+      <div ref={ref} className="z-10 relative aspect-[1.91/1] bg-zinc-800 rounded-t-[11px] overflow-hidden">
+        { isVisible && <img className="object-cover size-full m-auto animate-fade-in" src={bookmark.image} alt={bookmark.title} loading="lazy" onError={addImageFallback} /> }
         { bookmark.pinned && <PinBadge /> }
       </div>
       <div className="px-3 pb-3">
         <div className="flex items-center gap-2">
-          <div ref={faviconRef as React.RefObject<HTMLDivElement>} className="size-4 shrink-0">
-            <img className={`size-4 duration-300 rounded-sm ${bookmarkFaviconIsLoading ? 'opacity-0' : 'opacity-100'}`} src={faviconSrc} alt={bookmark.title} />
-          </div>
+          { isVisible && <img src={`https://icon.horse/icon/${bookmark.domain}`} alt={`${bookmark.title} icon`} className="size-4 shrink-0 rounded-sm" onError={addImageFallback} /> }
           <p className="font-medium truncate">{bookmark.title}</p>
-        <BookmarkOptions bookmark={bookmark} />
+          <BookmarkOptions bookmark={bookmark} />
         </div>
         <a href={bookmark.url} target="_blank" className="inline-block max-w-[75%] mb-2 text-sm truncate outline-none text-zinc-500">{bookmark.url}</a>
         <BookmarkTags bookmark={bookmark} />
---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef, useState } from 'react'
+
+const useIntersectionObserver = (options = {}) => {
+  const [ isVisible, setIsVisible ] = useState<boolean>(false)
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const node = ref.current
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsVisible(entry.isIntersecting)
+    }, {
+      root: null,
+      rootMargin: '800px',
+      threshold: 0.1,
+      ...options
+    })
+
+    if (node) observer.observe(node)
+
+    return () => {
+      if (node) observer.unobserve(node)
+    }
+  }, [options])
+
+  return { ref, isVisible }
+}
+
+export default useIntersectionObserver
